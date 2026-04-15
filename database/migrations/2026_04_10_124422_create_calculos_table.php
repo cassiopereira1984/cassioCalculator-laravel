44008c901f73db376b2c41fad64383fbe59a7325
Cassio Pacheco Pereira <159780983+cassiopereira1984@users.noreply.github.com>
2026-04-15 10:16:25 +0000

    fix corrgir migracion y anadir archivo sqlite

--- database/migrations/2026_04_10_124422_create_calculos_table.php
+++ database/migrations/2026_04_10_124422_create_calculos_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('calculos', function (Blueprint $table) {
             $table->id();
+            $table->decimal('numero1', 15, 4);
+            $table->decimal('numero2', 15, 4);
+            $table->string('operacion');
+            $table->decimal('resultado', 15, 4)->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('calculos');
